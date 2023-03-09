6392d86216a58e920231f8de3fdd46bb330683da
Rafael Londoño Botero <athenapms12345@gmail.com>
2023-03-09 17:19:04 -0500

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import  {UserForm}  from './src/components/UserForm';
+import { Modal, Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false)
+  const [modalUserForm,setModalUserForm] = useState(false)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
+        Registrate en la {""}
+        <Text style={styles.titleBold}>UAM</Text>
+      </Text>
+
+      <Pressable onPress={() => {setModalVisible(true)}} style = {styles.btnNewUser}>
+        <Text style = {styles.title}>Prueba Modal</Text>
+      </Pressable>
+
+      <Pressable onPress={() => {setModalUserForm(true)}} style = {styles.btnNewUser}>
+        <Text style = {styles.title}>Nuevo Usuario</Text>
+      </Pressable>
+      <UserForm modalUserForm={modalUserForm} setModalUserForm={setModalUserForm}></UserForm>
+
+      <Modal animationType='slide' visible={modalVisible}>
+        <Text>Desde Modal</Text>
+      </Modal>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor : "#0069a3",
+    flex : 1
   },
+  title : {
+    textAlign : "center",
+    fontSize : 22,
+    color : "#FFFFFF"
+  },
+  titleBold : {
+    fontWeight : "900",
+    color : "#f4d73b"
+  },
+  btnNewUser : {
+    backgroundColor : "#f4d73b",
+    padding : 10,
+    marginTop : 30,
+    marginHorizontal : 20,
+    borderRadius : 10
+  },
+  titleButton:{
+    textAlign : "center",
+    fontSize: 20,
+    color : "#000000"
+  }
 });
+
---
+++ src/components/UserForm.js
@@ -0,0 +1,40 @@
+import React,{useState} from 'react';
+import {ImageBackground, StyleSheet, Text, View,Modal} from 'react-native';
+
+const image = {uri: 'https://reactjs.org/logo-og.png'};
+
+export const UserForm = ({modalUserForm, setModalUserForm}) => {
+  return (
+    <Modal animationType="slide" visible={modalUserForm}>
+        <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+          <Text style={styles.text}>Inside</Text>
+        </ImageBackground>
+    </Modal>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    image: {
+      justifyContent: 'center',
+      position : "absolute",
+      marginTop : 0,
+      top : 0,
+      bottom : 0,
+      left : 0,
+      right : 0,
+      opacity : 0.5,
+      backgroundColor : 'rgba(52,52,52,0.8)'
+    },
+    text: {
+      color: 'white',
+      fontSize: 42,
+      lineHeight: 84,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      backgroundColor: '#000000c0',
+    },
+});
+  
