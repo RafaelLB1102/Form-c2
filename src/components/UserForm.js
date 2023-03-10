941906ab2176b8c5b8aae4dc63593a7ea08db724
Rafael Londoño Botero <athenapms12345@gmail.com>
2023-03-09 21:50:54 -0500

    clase 3/9/2023

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,13 +1,75 @@
 import React,{useState} from 'react';
-import {ImageBackground, StyleSheet, Text, View,Modal} from 'react-native';
+import {ImageBackground, StyleSheet, Text, View,Modal, Image, ScrollView, TextInput, Pressable} from 'react-native';
+import DatePicker from 'react-native-date-picker';
 
-const image = {uri: 'https://reactjs.org/logo-og.png'};
 
-export const UserForm = ({modalUserForm, setModalUserForm}) => {
+export const UserForm = ({modalUserForm}) => {
+  const [date, setDate] = useState(new Date());
+  const [open, setOpen] = useState(false);
+  
   return (
     <Modal animationType="slide" visible={modalUserForm}>
-        <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-          <Text style={styles.text}>Inside</Text>
+        <ImageBackground source={require("../assets/jpg/dev2.jpg")} resizeMode="cover" style={styles.backCover}>
+          <Image style = {styles.image} 
+          source={require("../assets/png/Logos_UAM-08.png")} 
+          />
+
+          <ScrollView>
+            <Text style={styles.title}>
+              Inscripción {""}
+              <Text style={styles.titleBold}>Vacaciones UAM</Text>
+            </Text>
+
+            <View style={styles.campo}>
+              <Text style={styles.text}>Nuevo usuario</Text>
+              <TextInput placeholder='Nombre Completo'
+                placeholderTextColor={"#F8F9F9"}
+                style = {styles.input}
+              >
+              </TextInput>
+            </View>
+
+            <View style = {styles.campo}>
+              <Text style={styles.text}>Correo</Text>
+                <TextInput placeholder='@autonoma.edu.co'
+                  placeholderTextColor={"#F8F9F9"}
+                  style = {styles.input}
+                  keyboardType="email-address"
+                >
+              </TextInput>
+            </View>
+
+            <View style = {styles.campo}>
+              <Text style={styles.text}>Celular</Text>
+                <TextInput placeholder='Celular'
+                  placeholderTextColor={"#F8F9F9"}
+                  style = {styles.input}
+                  keyboardType="phone-pad"
+                >
+              </TextInput>
+            </View>
+
+            <View style = {styles.campo}>
+              <Pressable onPress={() => setOpen(true)}>
+                <Text style={styles.text}>Fecha Inscripción</Text>
+              </Pressable>
+              <DatePicker
+                modal
+                open={open}
+                date={date}
+                mode={"date"}
+                onConfirm={(date) => {
+                  setOpen(false);
+                  setDate(date);
+                }}
+                onCancel={() => {
+                  setOpen(false);
+                }}
+            />
+            </View>
+
+          </ScrollView>
+
         </ImageBackground>
     </Modal>
   )
@@ -18,23 +80,52 @@ const styles = StyleSheet.create({
       flex: 1,
     },
     image: {
-      justifyContent: 'center',
+      margin : 15,
+      width : 75,
+      height : 75,
+      marginBottom : 15
+    },
+    backCover : {
       position : "absolute",
       marginTop : 0,
-      top : 0,
-      bottom : 0,
-      left : 0,
-      right : 0,
-      opacity : 0.5,
-      backgroundColor : 'rgba(52,52,52,0.8)'
+      top:0,
+      bottom:0,
+      left:0,
+      right:0,
+      opacity : 0.7,
+      backgroundColor:"rgba(52,52,52,alpha)"
     },
-    text: {
-      color: 'white',
-      fontSize: 42,
-      lineHeight: 84,
-      fontWeight: 'bold',
-      textAlign: 'center',
-      backgroundColor: '#000000c0',
+    title:{
+      textAlign : "center",
+      fontSize:22,
+      color:"#FFFFFF",
+      marginHorizontal : 30,
+      fontWeight : "600",
+      marginBottom : 20
     },
+    titleBold : {
+      textAlign : "center",
+      fontSize:22,
+      color:"#0069a3",
+      marginHorizontal : 30,
+      fontWeight : "600",
+      marginBottom : 20
+    },
+    input : {
+      backgroundColor : "#000000c0",
+      padding : 15,
+      borderRadius : 10,
+      marginBottom : 10
+    },
+    text : {
+      fontSize : 20,
+      color : "#FFF",
+      marginBottom : 8,
+      marginTop:12
+    },
+    campo : {
+      marginHorizontal : 30
+    }
+    
 });
   
